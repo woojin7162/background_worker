3cd3acc9c22258926b0ab4d360c8a86bb103b75d
woojin7162 <woojin7162@naver.com>
2025-05-23 13:35:15 +0900

    Update background_worker.py

--- background_worker.py
+++ background_worker.py
@@ -16,11 +16,13 @@ collection = db["scheduled_messages"]
 def send_notilab_push(body):
     to_nickname = "Shift_Alarm"  # notilab 앱 닉네임
     title = "교대근무 알리미"
+    sckey = "89150194-88f3-4b84-ac93-6f9b4fa91ce9"
     url = (
         "https://noti.kyulabs.app/send?"
         f"to={urllib.parse.quote(to_nickname)}"
         f"&title={urllib.parse.quote(title)}"
         f"&body={urllib.parse.quote(body)}"
+        f"&secretKey={urllib.parse.quote(sckey)}"
     )
     try:
         resp = requests.get(url, timeout=5)
